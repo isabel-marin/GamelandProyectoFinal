1edd6a2071b0f4b417435547dda47aa7f780a45a
isabel-marin <67794400+isabel-marin@users.noreply.github.com>
2020-09-01 17:16:11 +0200

    añado public usuario login y quito el nick

--- src/gameland/modelo/UsuarioDAO.java
+++ src/gameland/modelo/UsuarioDAO.java
@@ -14,7 +14,7 @@ public class UsuarioDAO {
 	private ResultSet rs;
 	
 	public void altaUsuario (Usuario u) throws SQLException {
-		String usuario = "INSERT INTO usuarios (idusuarios, nombreusuarios, apellidos, dni, telefono, codigopostal, ciudad, nick, email, pass, rol) VALUES (?,?,?,?,?,?,?,?,?,?,?)";
+		String usuario = "INSERT INTO usuarios (idusuarios, nombreusuarios, apellidos, dni, telefono, codigopostal, ciudad, email, pass, rol) VALUES (?,?,?,?,?,?,?,?,?,?)";
 		con = Conexion.getInstance().getConnection();
 		pst = con.prepareStatement(usuario);
 		pst.setInt(1, u.getIdusuarios());
@@ -24,15 +24,14 @@ public class UsuarioDAO {
 		pst.setString(5, u.getTelefono());
 		pst.setString(6, u.getCodigopostal());
 		pst.setString(7, u.getCiudad());
-		pst.setString(8, u.getNick());
-		pst.setString(9, u.getEmail());
-		pst.setString(10, u.getPass());
-		pst.setString(11, u.getRol());
+		pst.setString(8, u.getEmail());
+		pst.setString(9, u.getPass());
+		pst.setString(10, u.getRol());
 	
 		pst.executeUpdate();
 	}
 	public void modificarUsuario (Usuario u) throws SQLException {
-		String usuario = "UPDATE usuarios SET nombreusuarios = ?, apellidos = ?, dni = ?, telefono = ?, codigopostal = ?, ciudad = ?, nick = ?, email = ?, pass = ?, rol = ? WHERE idusuarios = ?";
+		String usuario = "UPDATE usuarios SET nombreusuarios = ?, apellidos = ?, dni = ?, telefono = ?, codigopostal = ?, ciudad = ?, email = ?, pass = ?, rol = ? WHERE idusuarios = ?";
 		con = Conexion.getInstance().getConnection();
 		pst = con.prepareStatement(usuario);
 		pst.setString(1, u.getNombreusuarios());
@@ -41,11 +40,10 @@ public class UsuarioDAO {
 		pst.setString(4, u.getTelefono());
 		pst.setString(5, u.getCodigopostal());
 		pst.setString(6, u.getCiudad());
-		pst.setString(7, u.getNick());
-		pst.setString(8, u.getEmail());
-		pst.setString(9, u.getPass());
-		pst.setString(10, u.getRol());
-		pst.setInt(11, u.getIdusuarios());
+		pst.setString(7, u.getEmail());
+		pst.setString(8, u.getPass());
+		pst.setString(9, u.getRol());
+		pst.setInt(10, u.getIdusuarios());
 		
 		pst.executeUpdate();
 	}
@@ -56,6 +54,27 @@ public class UsuarioDAO {
 		pst.setInt(1, idusuarios);
 		
 		pst.executeUpdate();
+	}
+	public Usuario login(String email, String pass) throws SQLException {
+		Usuario u = null;
+		String usuario = "SELECT nombreusuarios, apellidos, email, pass, rol FROM usarios WHERE email = ? AND pass = ?";
+		con = Conexion.getInstance().getConnection();
+		pst = con.prepareStatement(usuario);
+		pst.setString(1, email);
+		pst.setString(2, pass);
+		
+		rs = pst.executeQuery();
+		
+		if (rs.next()) {
+			u = new Usuario();
+			u.setNombreusuarios(rs.getString("nombreusuarios"));
+			u.setApellidos(rs.getString("apellidos"));
+			u.setEmail(rs.getString("email"));
+			u.setPass(rs.getString("pass"));
+			u.setRol(rs.getString("rol"));
+			
+		}
+		return u;
 	}
 
 }
