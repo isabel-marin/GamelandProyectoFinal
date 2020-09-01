54bc74aa5eb604ef469c4a83f711cbfc2c087d54
IustinPE <67796463+IustinPE@users.noreply.github.com>
2020-09-01 18:36:18 +0200

    Controlador.Logout
    El logout del package del controlador

--- src/gameland/controlador/Logout.java
+++ src/gameland/controlador/Logout.java
@@ -1,11 +1,14 @@
 package gameland.controlador;
 
 import java.io.IOException;
+
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
 /**
  * Servlet implementation class Logout
@@ -26,8 +29,14 @@ public class Logout extends HttpServlet {
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		response.getWriter().append("Served at: ").append(request.getContextPath());
+		HttpSession session = request.getSession(false);
+		
+		if(session!= null) {
+			
+			session.invalidate();
+			RequestDispatcher dispatcher = request.getRequestDispatcher("inicio.jsp");
+			dispatcher.forward(request, response);
+		}
 	}
 
 	/**
@@ -36,6 +45,6 @@ public class Logout extends HttpServlet {
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
 		doGet(request, response);
-	}
+	}  
 
 }
