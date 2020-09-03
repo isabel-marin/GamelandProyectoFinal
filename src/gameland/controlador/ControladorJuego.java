1029af3d96092784324a20401d22f2ea250ab4b5
IustinPE <67796463+IustinPE@users.noreply.github.com>
2020-09-03 18:44:43 +0200

    ControladorJuego
    Añadido el controlador para dar de alta juegos modificar y borrar

--- src/gameland/controlador/ControladorJuego.java
+++ src/gameland/controlador/ControladorJuego.java
@@ -1,11 +1,17 @@
 package gameland.controlador;
 
 import java.io.IOException;
+import java.sql.SQLException;
+
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+
+import gameland.modelo.Juego;
+import gameland.modelo.JuegoDAO;
 
 /**
  * Servlet implementation class ControladorJuego
@@ -26,16 +32,75 @@ public class ControladorJuego extends HttpServlet {
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		response.getWriter().append("Served at: ").append(request.getContextPath());
+		String opcion = request.getParameter("opcion");
+		int idjuegos = Integer.parseInt(request.getParameter("idjuegos"));
+		JuegoDAO jueDAO = new JuegoDAO();
+		
+		Juego j = null;
+		
+		String pagDest = "modificarJuego.jsp";
+		
+		switch(opcion) {
+			case "e":
+				try {
+					j = jueDAO.getJuego(idjuegos);
+					request.setAttribute("idjuegos",j);
+					pagDest = "modificarJuego.jsp";
+					
+				} catch (SQLException e) {
+					e.printStackTrace();
+				}
+				break;
+			case "b":
+				try {
+					jueDAO.borrarJuego(idjuegos);
+					
+				} catch(SQLException e) {
+					e.printStackTrace();
+					
+				}
+				break;
+		}
+		RequestDispatcher dispatcher = request.getRequestDispatcher(pagDest);
+		dispatcher.forward(request, response);
+		
 	}
 
 	/**
 	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		doGet(request, response);
+		//idjuegos, nombrejuegos, precio, descripcion
+		int idjuegos = Integer.parseInt(request.getParameter("idjuegos"));
+		String nombrejuegos = request.getParameter("nombrejuegos");
+		String precio = request.getParameter("precio");
+		String descripcion = request.getParameter("descripcion");
+		String opcion = request.getParameter("opcion");
+		Juego j = new Juego();
+		
+		j.setIdjuegos(idjuegos);
+		j.setNombrejuegos(nombrejuegos);
+		j.setPrecio(precio);
+		j.setDescripcion(descripcion);
+		
+		JuegoDAO juDAO = new JuegoDAO();
+		
+		String pagDest = "modificarJuego.jsp";
+		
+		try {
+			if(opcion == null) {
+				juDAO.altaJuego(j);
+				
+			}else {
+				juDAO.modificarJuego(j);
+				pagDest = "modificarJuego.jsp";
+			}
+				
+		}catch(SQLException e) {
+			e.printStackTrace();
+		}
+		RequestDispatcher dispatcher = request.getRequestDispatcher(pagDest);
+		dispatcher.forward(request, response);
 	}
 
 }
